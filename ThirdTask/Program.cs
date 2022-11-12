38f451c2ff5b8b8722016d30c480fb4033eea2c0
RenatDavletshin <immortal1307@gmail.com>
2022-11-12 20:30:07 +0500

    ThirdTaskEnd

---
+++ ThirdTask/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая задает массив из 8 элементов и выводит их на экран.
+
+Console.WriteLine("Введите число: ");
+int len = int.Parse(Console.ReadLine());
+int[]array = new int[len];
+for (int i = 0; i < len; i++)
+{
+   array[i] = new Random().Next(len);
+   Console.Write(array[i] + ", ");
+}   
