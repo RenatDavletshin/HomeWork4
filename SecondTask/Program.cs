580d9bd416ec4d77e408a66ed5383d6ceff710a4
RenatDavletshin <immortal1307@gmail.com>
2022-11-12 17:52:56 +0500

    SecondTaskEnd

---
+++ SecondTask/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число и выдает сумму цифр в числе.
+
+Console.WriteLine("Введите число: ");
+int count = int.Parse(Console.ReadLine());
+int sum = 0;
+
+while (count > 0)
+{
+    sum += count % 10; 
+    count /= 10;
+}
+Console.WriteLine($"Сумма всех цифр в числе равна: {sum}");
