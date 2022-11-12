a47e7910aa8576bd263109c17bd3536eba98129c
RenatDavletshin <immortal1307@gmail.com>
2022-11-12 15:52:07 +0500

    FirstTaskEnd

---
+++ FirstTask/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine("Введите число A");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int B = int.Parse(Console.ReadLine());
+int degree = A;
+for (int i = 1; i < B; i++)
+{
+    degree = degree * A;
+}
+Console.WriteLine("A в степени B равно: " + degree);
